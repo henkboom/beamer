965700dd0dbffeb1102af5c5545cdc9ae3fdaaa4
Henk Boom <henk@henk.ca>
2012-09-26 23:53:49 -0400

    Transform.changed event and RelativeTransform

---
+++ RelativeTransform.lua
@@ -0,0 +1,85 @@
+--- RelativeTransform
+--- =================
+---
+--- Implements a read-only but otherwise Transform-compatible interface,
+--- actually representing the composition of two Transforms.
+
+local class = require 'class'
+local Event = require 'Event'
+local Transform = require 'Transform'
+
+local RelativeTransform = class('RelativeTransform')
+
+function RelativeTransform:_init(parent_transform, local_transform)
+  self.changed = Event()
+
+  -- making this method a closure lets us use it as an event handler
+  function self._invalidate()
+    self._position = false
+    self._orientation = false
+    self.changed(self)
+  end
+
+  self._parent_transform = false
+  self._local_transform = false
+
+  self.parent_transform = parent_transform or Transform()
+  self.local_transform = local_transform or Transform()
+
+  self.changed(self)
+end
+
+-- relative_transform.parent_transform
+function RelativeTransform:get_parent_transform()
+  return self._parent_transform
+end
+function RelativeTransform:set_parent_transform(parent_transform)
+  if self._parent_transform ~= parent_transform then
+    if self._parent_transform then
+      self._parent_transform.changed:remove_handler(self._invalidate)
+    end
+    self._parent_transform = parent_transform;
+    if self._parent_transform then
+      self._parent_transform.changed:add_handler(self._invalidate)
+    end
+    self:_invalidate()
+  end
+end
+
+-- relative_transform.local_transform
+function RelativeTransform:get_local_transform()
+  return self._local_transform
+end
+function RelativeTransform:set_local_transform(local_transform)
+  if self._local_transform ~= local_transform then
+    if self._local_transform then
+      self._local_transform.changed:remove_handler(self._invalidate)
+    end
+    self._local_transform = local_transform;
+    if self._local_transform then
+      self._local_transform.changed:add_handler(self._invalidate)
+    end
+    self:_invalidate()
+  end
+end
+
+-- relative_transform.position (read only, computed lazily)
+function RelativeTransform:get_position()
+  if not self._position then
+    self._position = self._parent_transform.position +
+      self._parent_transform.orientation:rotate_vector(
+        self.local_transform.position)
+  end
+  return self._position
+end
+
+-- relative_transform.orientation (read only, computed lazily)
+function RelativeTransform:get_orientation()
+  if not self._orientation then
+    self._orientation =
+      self.parent_transform.orientation * self.local_transform.orientation
+  end
+  return self._orientation
+end
+
+return RelativeTransform
--- Transform.lua
+++ Transform.lua
@@ -1,12 +1,41 @@
+--- Transform
+--- =========
+---
+--- Holds a position and an orientation. Notifies through an event on changes.
+
 local class = require 'class'
-local Vector = require 'Vector'
+local Event = require 'Event'
 local Quaternion = require 'Quaternion'
+local Vector = require 'Vector'
 
 local Transform = class('Transform')
 
 function Transform:_init(position, orientation)
-  self.position = position or Vector.zero
-  self.orientation = orientation or Quaternion.identity
+  self.changed = Event()
+  self._position = position or Vector.zero
+  self._orientation = orientation or Quaternion.identity
+end
+
+function Transform:get_position()
+  return self._position
+end
+
+function Transform:set_position(position)
+  if self._position ~= position then
+    self._position = position
+    self.changed(self)
+  end
+end
+
+function Transform:get_orientation()
+  return self._orientation
+end
+
+function Transform:set_orientation(orientation)
+  if self._orientation ~= orientation then
+    self._orientation = orientation
+    self.changed(self)
+  end
 end
 
 return Transform
