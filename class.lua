da985582dea0d3ec5e881d43389d3ac644742f14
Henk Boom <henk@henk.ca>
2012-11-16 18:50:56 -0500

    improved getter/setter lookup, class property support

--- class.lua
+++ class.lua
@@ -3,8 +3,6 @@ local function class(name, base)
 
   local c = {}
 
-  local real_ctor
-
   if base then
     for k,v in pairs(base) do
       c[k] = v
@@ -12,60 +10,57 @@ local function class(name, base)
 
     c._init = nil
     c._base = base
-    real_ctor = function (obj, ...)
-      if c._init then
-        obj.super = base._ctor
+    c._ctor = function (obj, ...)
+      if rawget(c, '_init') then
+        rawset(obj, 'super', base._ctor)
         c._init(obj, ...)
-        obj.super = nil
+        rawset(obj, 'super', nil)
       else
         base._ctor(obj, ...)
       end
     end
   else
-    real_ctor = function (obj, ...)
+    c._ctor = function (obj, ...)
       if c._init then
         c._init(obj, ...)
       end
     end
   end
 
+  -- nil: unknown
+  -- false: there isn't one
   local getters = {}
   local setters = {}
 
-  c._ctor = function (...)
-    for k,v in pairs(c) do
-      local name = string.match(k, '^get_(.*)$')
-      if name then getters[name] = v end
-      local name = string.match(k, '^set_(.*)$')
-      if name then setters[name] = v end
-    end
-
-    c._ctor = real_ctor
-    real_ctor(...)
-  end
-
   c.__index = function (self, k)
-    local getter = getters[k]
-
-    if getter == nil then
-      local val = c[k]
-      if val == nil then
+    -- first check class
+    if rawget(c, k)  ~= nil then
+      return rawget(c, k)
+    elseif type(k) == 'string' then
+      -- then check class getters
+      if getters[k] == nil and type(k) == 'string' then
+        getters[k] = rawget(c, 'get_' .. k) or false
+      end
+      -- if there isn't a getter then we've got an error
+      if not getters[k] then
         error('field ' .. tostring(k) .. ' is not declared on ' ..
               tostring(self), 2)
       end
-      return val
+      -- invoke the getter
+      return getters[k](self)
     end
-
-    return getter(self)
   end
 
   c.__newindex = function (self, k, v)
-    local setter = setters[k]
+    -- check for a setter
+    if setters[k] == nil and type(k) == 'string' then
+      setters[k] = rawget(c, 'set_' .. k) or false
+    end
 
-    if setter == nil then
-      rawset(self, k, v)
+    if setters[k] then
+      setters[k](self, v)
     else
-      setter(self, v)
+      rawset(self, k, v)
     end
   end
 
@@ -81,6 +76,8 @@ local function class(name, base)
     return getmetatable(value) == c
   end
 
+  c.class = c
+
   setmetatable(c, {
     __call = function (_, ...)
       local obj = setmetatable({}, c)
@@ -89,7 +86,9 @@ local function class(name, base)
     end,
     __tostring = function ()
       return 'class ' .. c._name
-    end
+    end,
+    __index = c.__index,
+    __newindex = c.__newindex
   })
 
   return c
