d604b9b9cd041deeee57007f6d21b1b0e7f83f9e
Henk Boom <henk@henk.ca>
2012-11-19 17:09:45 -0500

    Collider bounds-check before real collision check

--- Collider.lua
+++ Collider.lua
@@ -11,18 +11,45 @@ function Collider:_init(transform, collision_shape)
   self.collision_shape = collision_shape
 end
 
+local function square(x)
+  return x*x
+end
+
+function Collider:get_bounds_center()
+  return self.transform:transform_point(self.collision_shape.bounds_center)
+end
+
+function Collider:get_bounds_radius()
+  return self.collision_shape.bounds_radius
+end
+
 function Collider:check_collision_with(other)
-  return collision.collision_check(
-    self.transform, function (...) return self.collision_shape:support(...) end,
-    other.transform, function (...) return other.collision_shape:support(...) end,
-    other.collision_shape.restricted_normals)
+  if (self.bounds_center - other.bounds_center):square_magnitude() <=
+     square(self.bounds_radius + other.bounds_radius)
+  then
+    return collision.collision_check(
+      self.transform, function (...) return self.collision_shape:support(...) end,
+      other.transform, function (...) return other.collision_shape:support(...) end,
+      other.collision_shape.restricted_normals)
+  else
+    return false
+  end
 end
 
 function Collider:sweep(sweep, other)
-  return collision.collision_sweep(
-    self.transform, function (...) return self.collision_shape:support(...) end,
-    sweep,
-    other.transform, function (...) return other.collision_shape:support(...) end)
+  local bounds_center = self.bounds_center + sweep * 0.5
+  local bounds_radius = self.bounds_radius + sweep:magnitude() * 0.5
+
+  if (bounds_center - other.bounds_center):square_magnitude() <=
+     square(bounds_radius + other.bounds_radius)
+  then
+    return collision.collision_sweep(
+      self.transform, function (...) return self.collision_shape:support(...) end,
+      sweep,
+      other.transform, function (...) return other.collision_shape:support(...) end)
+  else
+    return false
+  end
 end
 
 return Collider
--- MeshCollisionShape.lua
+++ MeshCollisionShape.lua
@@ -5,18 +5,45 @@
 
 local class = require 'class'
 local Vector = require 'Vector'
+local ffi = require 'ffi'
 
 local MeshCollisionShape = class('MeshCollisionShape')
 
+local vector_array_type = ffi.typeof('$[?]', Vector.type)
+
 function MeshCollisionShape:_init(vertices, restricted_normals)
-  self.vertices = vertices;
+  self.vertices = vector_array_type(#vertices, vertices)
+  self.vertex_count = #vertices
+
   self.restricted_normals = restricted_normals or false
+
+  local min = self.vertices[0]
+  local max = self.vertices[0]
+  for i = 1, self.vertex_count-1 do
+    min = Vector(
+      math.min(max.x, self.vertices[i].x),
+      math.min(max.y, self.vertices[i].y),
+      math.min(max.z, self.vertices[i].z))
+    max = Vector(
+      math.max(max.x, self.vertices[i].x),
+      math.max(max.y, self.vertices[i].y),
+      math.max(max.z, self.vertices[i].z))
+  end
+
+  self.bounds_center = (min + max) * 0.5
+
+  local radius_sqr = 0
+  for i = 0, self.vertex_count-1 do
+    radius_sqr = math.max(radius_sqr,
+      (self.vertices[i] - self.bounds_center):square_magnitude())
+  end
+  self.bounds_radius = math.sqrt(radius_sqr)
 end
 
 function MeshCollisionShape:support(direction)
   local point
   local distance = -math.huge
-  for i = 1, #self.vertices do
+  for i = 0, self.vertex_count-1 do
     local new_distance = Vector.dot(self.vertices[i], direction)
     if new_distance > distance then
       point = self.vertices[i]
