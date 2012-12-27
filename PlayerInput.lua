ad036a385273ff90d233cb63f677b95b4a4eaef3
Henk Boom <henk@henk.ca>
2012-12-26 22:38:03 -0500

    radial steering controls

--- PlayerInput.lua
+++ PlayerInput.lua
@@ -26,6 +26,8 @@ function PlayerInput:_init(parent)
   self.direction = Vector.zero
   self.a = false
   self.b = false
+
+  self._delta = 0
   
   self.changed = Event()
 
@@ -42,15 +44,14 @@ local pointers_left = {}
 local pointers_right = {}
 
 function PlayerInput:handle_event(event)
-  if event.type == 'pointer_down' then
-    local video = require 'system.video'
-    local zone = event.x < self.game.video.width/2 and
-                 pointers_left or pointers_right
-    zone[event.id] = true
-  elseif event.type == 'pointer_up' or
-         event.type == 'pointer_cancel' then
-    pointers_left[event.id] = nil
-    pointers_right[event.id] = nil
+  if event.type == 'pointer_motion' then
+    local rect = self.game.video.viewport
+    local origin = Vector(rect.x + rect.w/2, rect.y + rect.h/2)
+    local old_pos = Vector(event.x-event.dx, event.y-event.dy) - origin
+    local pos = Vector(event.x, event.y) - origin
+    local angle = math.atan2(pos.y, pos.x) - math.atan2(old_pos.y, old_pos.x)
+    angle = (angle + math.pi) % (math.pi * 2) - math.pi
+    self._delta = self._delta + angle * 25
   end
   if event.type == 'key_down' then
     if event.key == 'left' then pointers_left.button = true end
@@ -62,7 +63,9 @@ function PlayerInput:handle_event(event)
 end
 
 function PlayerInput:preupdate()
-  self.direction = Vector((next(pointers_left) and -1 or 0) + (next(pointers_right) and 1 or 0), 0)
+  self.direction = Vector(
+    (next(pointers_left) and -1 or 0) + (next(pointers_right) and 1 or 0) + self._delta, 0)
+  self._delta = self._delta * 0.7
 
   self.a = true
   self.b = false
