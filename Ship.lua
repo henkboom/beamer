e504ee38fa174e7492b100783e90d85304174fc7
Henk Boom <henk@henk.ca>
2013-01-31 00:16:49 -0500

    headlights

--- Ship.lua
+++ Ship.lua
@@ -15,4 +15,18 @@ return blueprint('Ship', 'Component', {
       -0.3, 0.5, 0
     }
   }}}},
+  {'headlight_renderer', {'graphics.MeshRenderer', 'self'}},
+  {'headlight_renderer.transform', 'self.transform'},
+  {'headlight_renderer.material', {'materials.Basic'}},
+  {'headlight_renderer.material.uniforms.color', '{0.1, 0.1, 0.1}'},
+  {'headlight_renderer.material.blend_dst', '"one"'},
+  {'headlight_renderer.mesh', {'graphics.Mesh', {{
+    elements = {0,1,2, 2,3,0},
+    position = {
+      -0.3, 0.7, 0,
+       0.3, 0.7, 0,
+       0.6, 2.0, 0,
+      -0.6, 2.0, 0
+    }
+  }}}},
 })
