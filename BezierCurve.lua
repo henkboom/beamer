b37480aaeb0ebb859a6593ae790d090c1c952af1
Henk Boom <henk@henk.ca>
2013-03-30 03:36:48 -0400

    BezierCurves now immutable

--- BezierCurve.lua
+++ BezierCurve.lua
@@ -1,18 +1,23 @@
 --- BezierCurve
 --- ===========
 ---
---- An implementation of quadratic Bezier curves.
+--- An implementation of quadratic Bezier curves. Immutable.
 
 local class = require 'class'
 
 local BezierCurve = class('BezierCurve')
 
 function BezierCurve:_init(p0, p1, p2, p3)
-  self.p0 = p0
-  self.p1 = p1
-  self.p2 = p2
-  self.p3 = p3
+  self._p0 = p0
+  self._p1 = p1
+  self._p2 = p2
+  self._p3 = p3
 end
+
+function BezierCurve:get_p0() return self._p0 end
+function BezierCurve:get_p1() return self._p1 end
+function BezierCurve:get_p2() return self._p2 end
+function BezierCurve:get_p3() return self._p3 end
 
 function BezierCurve:__tostring()
   return string.format('BezierCurve(%s, %s, %s, %s)',
@@ -22,23 +27,23 @@ end
 function BezierCurve:subdivide(t)
   t = t or 0.5
   local invt = 1 - t
-  local p01 =   self.p0 * invt + self.p1 * t
-  local p12 =   self.p1 * invt + self.p2 * t
-  local p23 =   self.p2 * invt + self.p3 * t
-  local p012 =      p01 * invt +     p12 * t
-  local p123 =      p12 * invt +     p23 * t
-  local p0123 =    p012 * invt +    p123 * t
-  return BezierCurve(self.p0, p01, p012, p0123),
-         BezierCurve(p0123, p123, p23, self.p3)
+  local p01 =   self._p0 * invt + self._p1 * t
+  local p12 =   self._p1 * invt + self._p2 * t
+  local p23 =   self._p2 * invt + self._p3 * t
+  local p012 =       p01 * invt +      p12 * t
+  local p123 =       p12 * invt +      p23 * t
+  local p0123 =     p012 * invt +     p123 * t
+  return BezierCurve(self._p0, p01, p012, p0123),
+         BezierCurve(p0123, p123, p23, self._p3)
 end
 
 function BezierCurve:adaptive_subdivision(max_error, output)
   max_error = max_error or 0.1
   output = output or {}
 
-  local line = self.p3 - self.p0
-  local ctl1 = self.p1 - self.p0
-  local ctl2 = self.p2 - self.p3
+  local line = self._p3 - self._p0
+  local ctl1 = self._p1 - self._p0
+  local ctl2 = self._p2 - self._p3
   if (ctl1 - ctl1:project(line)):square_magnitude() <= max_error*max_error and
      (ctl2 - ctl2:project(line)):square_magnitude() <= max_error*max_error then
     output[#output+1] = self
